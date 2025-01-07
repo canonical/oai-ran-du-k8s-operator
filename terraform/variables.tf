05e6e6abedc9b8d24797e1f721ef0ae5db24c329
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2025-01-07 16:28:15 -0500

    chore: Update Terraform module to follow CC006

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,5 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
-
-variable "model_name" {
-  description = "Name of Juju model to deploy application to."
-  type        = string
-  default     = ""
-}
 
 variable "app_name" {
   description = "Name of the application in the Juju model."
@@ -23,4 +17,39 @@ variable "config" {
   description = "Application config. Details about available options can be found at https://charmhub.io/oai-ran-du-k8s/configuration."
   type        = map(string)
   default     = {}
+}
+
+variable "constraints" {
+  description = "Juju constraints to apply for this application."
+  type        = string
+  default     = "arch=amd64"
+}
+
+variable "model" {
+  description = "Reference to a `juju_model`."
+  type        = string
+  default     = ""
+}
+
+variable "resources" {
+  description = "Resources to use with the application. Details about available options can be found at https://charmhub.io/oai-ran-du-k8s/configure."
+  type        = map(string)
+  default     = {}
+}
+
+variable "revision" {
+  description = "Revision number of the charm"
+  type        = number
+  default     = null
+}
+
+variable "units" {
+  description = "Number of units to deploy"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.units == 1
+    error_message = "Scaling is not supported for this charm."
+  }
 }
