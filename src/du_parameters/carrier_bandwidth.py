82e32d94fd9c8395099c5828c7728245ce247af2
Gulsum Atici <gulsum.atici@canonical.com>
2025-02-18 02:32:27 +0300

    feat:  calculate carrier bandwidth (#86)
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ src/du_parameters/carrier_bandwidth.py
@@ -0,0 +1,91 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Calculate Carrier Bandwidth using bandwidth and subcarrier spacing."""
+
+import decimal
+import logging
+from decimal import Decimal
+
+from src.du_parameters.frequency import (
+    Frequency,
+)
+from src.du_parameters.guard_band import GuardBandError, get_minimum_guard_band
+
+logger = logging.getLogger(__name__)
+
+CONFIG_CONSTANT_TWO = Decimal("2")
+NUMBER_OF_SUBCARRIERS = Decimal("12")
+
+
+class CarrierBandwidthError(Exception):
+    """Exception raised when carrier bandwidth calculation fails."""
+
+    pass
+
+
+def get_carrier_bandwidth(bandwidth: Frequency, subcarrier_spacing: Frequency) -> int:
+    """Calculate channel bandwidths of the carrier.
+
+    Result should be in terms of Resource Blocks (RB) using bandwidth and subcarrier spacing.
+
+    Args:
+        bandwidth (Frequency): bandwidth
+        subcarrier_spacing (Frequency): subcarrier spacing
+
+    Returns:
+        carrier bandwidth (int): The channel bandwidths of the carrier
+         in terms of Resource Blocks (RB)
+
+    Raises:
+        ValueError: If bandwidth or subcarrier spacing are not greater than 0.
+        CarrierBandwidthError: If calculation fails or Guard band calculation fails.
+    """
+    if subcarrier_spacing <= Frequency(0) or bandwidth <= Frequency(0):
+        logger.error("Both bandwidth and subcarrier spacing must be greater than 0.")
+        raise ValueError("Both bandwidth and subcarrier spacing must be greater than 0.")
+
+    try:
+        guard_band = get_minimum_guard_band(subcarrier_spacing, bandwidth)
+    except (TypeError, GuardBandError) as e:
+        logger.error(
+            "Guard band calculation failed for bandwidth: %s and subcarrier spacing: %s: %s",
+            bandwidth,
+            subcarrier_spacing,
+            str(e),
+        )
+        raise CarrierBandwidthError(
+            "Guard band calculation failed for bandwidth: %s and subcarrier spacing: %s: %s",
+            bandwidth,
+            subcarrier_spacing,
+            str(e),
+        )
+
+    try:
+        carrier_bandwidth = (
+            (bandwidth - CONFIG_CONSTANT_TWO * guard_band) / subcarrier_spacing
+        ) / NUMBER_OF_SUBCARRIERS
+        carrier_bandwidth_int = int(carrier_bandwidth)
+        logger.info(
+            "Calculated channel bandwidth for bandwidth=%s and subcarrier spacing=%s: %s",
+            bandwidth,
+            subcarrier_spacing,
+            carrier_bandwidth_int,
+        )
+        return carrier_bandwidth_int
+
+    except (
+        TypeError,
+        ValueError,
+        decimal.InvalidOperation,
+    ) as e:
+        logger.error(
+            "Error calculating carrier bandwidth for bandwidth=%s and subcarrier spacing=%s: %s",
+            bandwidth,
+            subcarrier_spacing,
+            str(e),
+        )
+        raise CarrierBandwidthError(
+            f"Error calculating carrier bandwidth for bandwidth={bandwidth}"
+            f" and subcarrier spacing={subcarrier_spacing}: {e}"
+        )
---
+++ src/du_parameters/guard_band.py
@@ -0,0 +1,103 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Define a dictionary where SCS values map to bandwidths and their guard bands."""
+
+import logging
+
+from src.du_parameters.frequency import Frequency
+
+logger = logging.getLogger(__name__)
+
+
+class GuardBandError(Exception):
+    """Exception raised when guard band calculation fails."""
+
+    pass
+
+
+_minimum_guard_bands = {
+    # SCS = 15 kHz
+    Frequency.from_khz(15): {
+        # Bandwidth             Guard Band
+        Frequency.from_mhz(5): Frequency.from_khz("242.5"),
+        Frequency.from_mhz(10): Frequency.from_khz("312.5"),
+        Frequency.from_mhz(15): Frequency.from_khz("382.5"),
+        Frequency.from_mhz(20): Frequency.from_khz("452.5"),
+        Frequency.from_mhz(25): Frequency.from_khz("552.5"),
+        Frequency.from_mhz(30): Frequency.from_khz("592.5"),
+        Frequency.from_mhz(40): Frequency.from_khz("552.5"),
+        Frequency.from_mhz(50): Frequency.from_khz("692.5"),
+    },
+    # SCS = 30 kHz
+    Frequency.from_khz(30): {
+        # Bandwidth             Guard Band
+        Frequency.from_mhz(5): Frequency.from_khz(505),
+        Frequency.from_mhz(10): Frequency.from_khz(665),
+        Frequency.from_mhz(15): Frequency.from_khz(645),
+        Frequency.from_mhz(20): Frequency.from_khz(805),
+        Frequency.from_mhz(25): Frequency.from_khz(785),
+        Frequency.from_mhz(30): Frequency.from_khz(945),
+        Frequency.from_mhz(40): Frequency.from_khz(905),
+        Frequency.from_mhz(50): Frequency.from_khz(1045),
+        Frequency.from_mhz(60): Frequency.from_khz(825),
+        Frequency.from_mhz(70): Frequency.from_khz(965),
+        Frequency.from_mhz(80): Frequency.from_khz(925),
+        Frequency.from_mhz(90): Frequency.from_khz(885),
+        Frequency.from_mhz(100): Frequency.from_khz(845),
+    },
+    # SCS = 60 kHz
+    Frequency.from_khz(60): {
+        # Bandwidth             Guard Band
+        Frequency.from_mhz(10): Frequency.from_khz(1010),
+        Frequency.from_mhz(15): Frequency.from_khz(990),
+        Frequency.from_mhz(20): Frequency.from_khz(1330),
+        Frequency.from_mhz(25): Frequency.from_khz(1310),
+        Frequency.from_mhz(30): Frequency.from_khz(1290),
+        Frequency.from_mhz(40): Frequency.from_khz(1610),
+        Frequency.from_mhz(50): Frequency.from_khz(1570),
+        Frequency.from_mhz(60): Frequency.from_khz(1530),
+        Frequency.from_mhz(70): Frequency.from_khz(1490),
+        Frequency.from_mhz(80): Frequency.from_khz(1450),
+        Frequency.from_mhz(90): Frequency.from_khz(1410),
+        Frequency.from_mhz(100): Frequency.from_khz(1370),
+    },
+}
+
+
+def get_minimum_guard_band(subcarrier_spacing: Frequency, bandwidth: Frequency) -> Frequency:
+    """Retrieve the minimum guard band for a given subcarrier spacing and bandwidth.
+
+    Args:
+        subcarrier_spacing (Frequency): The subcarrier spacing in Frequency
+        bandwidth (Frequency): The bandwidth in Frequency
+
+    Returns:
+        guard_band (Frequency): The minimum guard band in Frequency.
+
+    Raises:
+        GuardBandError: If the guard band is not found.
+    """
+    scs_data = _minimum_guard_bands.get(subcarrier_spacing)
+    if scs_data:
+        guard_band = scs_data.get(bandwidth)
+        if guard_band:
+            logger.info(
+                "Calculated minimum guard band for SCS=%s and bandwidth=%s: %s",
+                subcarrier_spacing,
+                bandwidth,
+                guard_band,
+            )
+            return guard_band
+
+        logger.error(
+            "Requested guard band for SCS=%s and bandwidth=%s not found.",
+            subcarrier_spacing,
+            bandwidth,
+        )
+        raise GuardBandError(
+            f"No guard band found for bandwidth={bandwidth} and SCS={subcarrier_spacing}."
+        )
+
+    logger.error("Requested guard band for SCS=%s not found.", subcarrier_spacing)
+    raise GuardBandError(f"No guard band found for SCS={subcarrier_spacing}.")
